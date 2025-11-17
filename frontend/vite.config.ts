535be747cec751a084794ea73a9c6216e4d2531f
jhuang676 <sorrower.keen@gmail.com>
2025-11-16 20:52:47 -0500

    add case sensitive base folder fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/paycheck-optimizer/', 
+  base: '/Paycheck-Optimizer/', 
   build: {
     outDir: '../docs', // Output to a 'docs' folder in the root
   },
